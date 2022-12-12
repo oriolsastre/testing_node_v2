d0ba4f1ef4461128ca59c483a80b607bfb427be5
Jonatan Vicente <it@jonatanvicente.com>
2022-12-12 12:54:40 +0100

    Adding a bit of testing

--- CocktailsAndDrinks_Mocks/app/__test__/cd-model-test.test.js
+++ CocktailsAndDrinks_Mocks/app/__test__/cd-model-test.test.js
@@ -1,5 +1,12 @@
+const config = require('../config/config');
+const express = require('express');
 const model = require('../model/cd-model');
+const dotenv = require("dotenv");
+const path = require("path");
 
-test('Assert retrieve data from Categories Table', () => {
-    expect(true).toBe(true);
+dotenv.config({ path: path.resolve(__dirname, 'test.env') });
+
+test('Assert retrieve data from Categories Table', async() => {
+    const data = await model.category(15);
+    expect(data.CATEGORIA).toBe("Whiskey");
 });
