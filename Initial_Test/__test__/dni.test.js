aada6db018e54a99aed9c684364bb53cc24c1682
Oriol Sastre <oriol.sastre@gmail.com>
2022-12-22 12:50:16 +0100

    dni

--- Initial_Test/__test__/dni.test.js
+++ Initial_Test/__test__/dni.test.js
@@ -5,7 +5,16 @@ test("DNIs correcte", () => {
     expect(lletraDNI('70810346')).toBe('Q')
     expect(lletraDNI('49277367')).toBe('M')
 })
+test("0 inicials han de retornar la lletra correcte, i són necessaris", () => {
+    expect(lletraDNI('04338003')).toBe('L')
+    expect(() => {
+        lletraDNI(4338003)
+    }).toThrow(Error)
+})
 test("DNI amb mida incorrecte", () => {
+    expect(() => {
+        lletraDNI()
+    }).toThrow(Error)
     expect(() => {
         lletraDNI(5)
     }).toThrow(Error)
@@ -19,7 +28,7 @@ test("DNI que no siguin numeros hauria de ser error", () => {
         lletraDNI('53298933K')
     }).toThrow(Error)
     expect(() => {
-        lletraDNI('1234567k')
+        lletraDNI('1234567e')
     }).toThrow(Error)
     expect(() => {
         lletraDNI('z')
